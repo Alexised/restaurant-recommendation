ecf51636a5fdadceaf4f4dfa1ee24a0382d1d05d
alexis <edwinalexis1996@gmail.com>
2023-05-26 10:14:44 -0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,7 @@ import * as mongoose from 'mongoose';
 
 async function bootstrap() {
   // Conectar a la base de datos MongoDB
-  await mongoose.connect('mongodb://localhost:27017/Restaurant', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  await mongoose.connect('mongodb://localhost:27017/Recomendador');
 
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
